d2b7c6501ca52251e80fec8bdebf286b867756db
Michael <michael.hamer.stanley@gmail.com>
2020-06-30 15:38:43 -0400

    Changes from original repo

--- tf_utils/tf_ops/pointnet2_ops/grouping/tf_grouping_compile.sh
+++ tf_utils/tf_ops/pointnet2_ops/grouping/tf_grouping_compile.sh
@@ -7,4 +7,4 @@
 # TF1.4
 # g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
-g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I /home/mmvc/anaconda2/envs/Xiang_Li/lib/python2.7/site-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /home/mmvc/anaconda2/envs/Xiang_Li/lib/python2.7/site-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L//home/mmvc/anaconda2/envs/Xiang_Li/lib/python2.7/site-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
+g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I$TF_INC  -I /usr/local/cuda-8.0/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
